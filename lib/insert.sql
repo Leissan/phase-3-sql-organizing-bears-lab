5757bd0635ff80a094627f45a9bcec6364cb66ef
Leissan Khan <leissan.khan@gmail.com>
2022-08-19 13:13:35 -0400

    commit tree 2e97372001d8d0a5830cd7cd82c05bebc30f09f1

---
+++ lib/insert.sql
@@ -0,0 +1,11 @@
+INSERT INTO 
+bears (id, name, age, sex, color, temperament, alive)
+VALUES 
+(1, "Mr. Chocolate", 2, "male", "brown", "mild", 1),
+(2, "Rowdy", 3, "male", "brown", "mild", 1),
+(3, "Tabitha", 3, "female", "white", "angry", 1),
+(4, "Sergeant Brown", 2, "male", "brown", "mild", 1),
+(5, "Melissa", 2, "female", "pink", "mild", 1),
+(6, "Grinch", 2, "male", "black", "mild", 1),
+(7, "Wendy", 2, "female", "brown", "mild", 1),
+(8, NULL, 2, "male", "brown", "mild", 1)
